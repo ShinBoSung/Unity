733a21570781a1a6ca1a967e232568ef59853b77
신보성 <kisioka1@gmail.com>
2022-11-17 13:34:09 +0900

    플랫포머 - 플레이어 점프 구현

--- Unity 2D Example/Unity 2D Example/Assets/script/PlayerMove.cs
+++ Unity 2D Example/Unity 2D Example/Assets/script/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxspeed;
+    public float JumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
@@ -18,6 +19,13 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
         //Stop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
@@ -59,5 +67,22 @@ public class PlayerMove : MonoBehaviour
         {
             rigid.velocity = new Vector2(maxspeed * (-1), rigid.velocity.y);
         }
+
+        //Landing Platform
+        if(rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    anim.SetBool("isJumping", false);
+                }
+            }
+        }
+        
     }
 }
