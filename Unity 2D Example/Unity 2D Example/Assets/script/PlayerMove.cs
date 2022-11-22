6968a1105555e16a7d1a623158aa44cedea60770
신보성 <kisioka1@gmail.com>
2022-11-22 16:18:52 +0900

    플랫포머 체력 및 사망 추가 포인트 이벤트 추가

--- Unity 2D Example/Unity 2D Example/Assets/script/PlayerMove.cs
+++ Unity 2D Example/Unity 2D Example/Assets/script/PlayerMove.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public GameManager gameManager;
     public float maxspeed;
     public float JumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
-    
+    CapsuleCollider2D capsulcollider;
+
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-        
+        capsulcollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -112,7 +114,23 @@ public class PlayerMove : MonoBehaviour
         if (collision.gameObject.tag == "Item")
         {
             //Point
+            bool isBronze = collision.gameObject.name.Contains("BronzeCoin");
+            bool isSilver = collision.gameObject.name.Contains("SilverCoin");
+            bool isGold = collision.gameObject.name.Contains("GoldCoin");
 
+            if(isBronze)
+            {
+                gameManager.stagePoint += 10;
+            }
+            else if(isSilver)
+            {
+                gameManager.stagePoint += 50;
+            }
+            else if (isGold)
+            {
+                gameManager.stagePoint += 100;
+            }
+                
             //Deactive Item
             collision.gameObject.SetActive(false);
         }
@@ -120,10 +138,13 @@ public class PlayerMove : MonoBehaviour
         else if (collision.gameObject.tag == "Finish")
         {
             //Next Stage
+            gameManager.NextStage();
         }
     }
     void OnAttack(Transform enemy)
     {
+        gameManager.stagePoint += 100;
+
         rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
 
         EnemyMove enemyMove = enemy.GetComponent<EnemyMove>();
@@ -132,13 +153,20 @@ public class PlayerMove : MonoBehaviour
 
     void OnDamaged(Vector2 targetPos)
     {
+        //Healt Down
+        gameManager.healthDown();
+
+        //Change Layer (Immortal Active)
         gameObject.layer = 9;
 
+        //view Alpha
         spriteRenderer.color = new Color(1, 1, 1, 0.4f);
 
+        //Reaction Force
         int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
         rigid.AddForce(new Vector2(dirc, 1) * 7, ForceMode2D.Impulse);
 
+        //Animation
         anim.SetTrigger("doDamaged");
 
         Invoke("offDamaged", 0.75f);
@@ -150,4 +178,15 @@ public class PlayerMove : MonoBehaviour
         spriteRenderer.color = new Color(1, 1, 1, 1);
     }
     
+    public void OnDie()
+    {
+        //Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        //Sprite Flip Y
+        spriteRenderer.flipY = true;
+        //Collider Disable
+        capsulcollider.enabled = false;
+        //Die Effect Jump
+        rigid.AddForce(Vector2.up * 7, ForceMode2D.Impulse);
+    }
 }
