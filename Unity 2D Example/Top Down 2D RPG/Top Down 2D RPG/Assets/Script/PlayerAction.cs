adc860878a96985083a0cf92a0475c9d87afcefe
신보성 <kisioka1@gmail.com>
2022-12-01 00:20:19 +0900

    맵 수정 및 캐릭터 이동 애니메이션 추가

--- Unity 2D Example/Top Down 2D RPG/Top Down 2D RPG/Assets/Script/PlayerAction.cs
+++ Unity 2D Example/Top Down 2D RPG/Top Down 2D RPG/Assets/Script/PlayerAction.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerAction : MonoBehaviour
 {
     public float Speed;
     float h;
     float v;
     bool isHorizonMove;
-
+    Animator anim;
+    Vector3 dirVec;
     Rigidbody2D rigid;
+    GameObject scanObject;
+
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,13 +35,57 @@ public class PlayerAction : MonoBehaviour
         bool vUp = Input.GetButtonUp("Vertical");
 
         //Check Horizontal Move
-        if (hDown || vUp)
+        if (hDown )
         {
             isHorizonMove = true;
         }
-        else if(vDown || hUp)
+        else if(vDown)
         {
             isHorizonMove = false;
+        }
+        else if(hUp||vUp)
+        {
+            isHorizonMove = h != 0;
+        }
+
+        //Animaion
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        } 
+        else if(anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            anim.SetBool("isChange", false);
+        }
+
+        //Direction
+        if(vDown && v ==1)
+        {
+            dirVec = Vector3.up;
+        }
+        else if (vDown && v == -1)
+        {
+            dirVec = Vector3.down;
+        }
+        else if (hDown && h == 1)
+        {
+            dirVec = Vector3.right;
+        }
+        else if (hDown && h == -1)
+        {
+            dirVec = Vector3.left;
+        }
+
+        //Scan Object
+        if(Input.GetButtonDown("Jump") && scanObject != null)
+        {
+            Debug.Log("this is : " + scanObject.name);
         }
     }
 
@@ -45,5 +94,18 @@ public class PlayerAction : MonoBehaviour
         //Move
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
         rigid.velocity = moveVec * Speed;
+
+        //Ray
+        Debug.DrawRay(rigid.position, dirVec *0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+        {
+            scanObject = null;
+        }
     }
 }
